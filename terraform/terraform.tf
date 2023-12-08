9908304cef9eaecaf22c197983da770f514eb734
Eric van Wijk <eric@van-wijk.com>
2023-12-08 13:02:33 +0100

    Remove deprecated attributes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -20,6 +20,7 @@ data external azdo_token {
   program                      = [
     "az", "account", "get-access-token", 
     "--resource", "499b84ac-1321-427f-aa17-267ca6975798", # Azure DevOps
+    "--query","{accessToken:accessToken}",
     "-o","json"
   ]
 }
