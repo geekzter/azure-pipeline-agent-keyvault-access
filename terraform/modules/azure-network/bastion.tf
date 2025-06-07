8d730bbd1303a3079f18869ef7b112c224aac94a
Eric van Wijk <eric@van-wijk.com>
2025-06-07 15:54:22 +0200

    Update providers

--- terraform/modules/azure-network/bastion.tf
+++ terraform/modules/azure-network/bastion.tf
@@ -184,7 +184,7 @@ resource azurerm_monitor_diagnostic_setting bastion_ip {
     category                   = "DDoSMitigationReports"
   }  
 
-  metric {
+  enabled_metric {
     category                   = "AllMetrics"
   }
 
